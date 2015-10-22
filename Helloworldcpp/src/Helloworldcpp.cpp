1890bd56650ccdcbb53f372aa37c7c15a1bd2397
Marcus <marcus.a.walters@ryerson.ca>
2015-10-22 18:42:58 -0400

    added numbers

---
+++ Helloworldcpp/src/Helloworldcpp.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Helloworldcpp.cpp
+// Author      : Mw
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World.8988!!!" << endl; // prints !!!Hello World.!!!
+	return 0;
+}
